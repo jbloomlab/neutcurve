b96bd247a6291c3b3f1748fc1bf12765b3dc30bb
jbloom <jbloom@fredhutch.org>
2019-03-29 09:18:45 -0700

    added `scipy` to requirements

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     long_description=readme,
     license='GPLv3',
     install_requires=[
-        'numpy>=1.13',
+        'scipy>=1.1.0',
         'pandas>=0.23',
         'matplotlib>=3.0.0',
         'plotnine>=0.5.1',
